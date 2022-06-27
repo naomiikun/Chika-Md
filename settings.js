266d696efa1ef106fc4989db4cba79f7ca17969e
naomiikun <81824896+naomiikun@users.noreply.github.com>
2022-06-27 19:26:22 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,8 +22,8 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6289656534945','6289656534945','6289656534945'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6289656534945'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
 global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
